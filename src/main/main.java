472fe784060bbc0b5e08b7d747c8b1f1cc6816b4
maurice <maurice@air-von-maurice.fritz.box>
2025-04-18 14:13:53 +0200

    Initial commit.

---
+++ src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class main {
+	public static void main(String[] args) {
+		int var = 0;
+		if (var == 0){
+			System.out.println("The number is 0");
+		} else if (var%2 == 0) {
+			System.out.println("The number is even!");
+		} else {
+			System.out.println("The number is odd");
+		}
+	}
+}
