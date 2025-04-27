8f0edd282ffef1e21aee2289ff8a32b20e7b8944
vivianalavie <viviana.littig@fs-students.de>
2025-04-27 18:36:13 +0200

    defining the controller

--- src/main/java/com/studyflow/controller/AuthController.java
+++ src/main/java/com/studyflow/controller/AuthController.java
@@ -1,14 +1,21 @@
 package com.studyflow.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/auth/register")
+    @Operation(summary = "Register a new user")
+    @PostMapping("/register")
     public String register() {
         return "not implemented";
     }
 
+    @Operation(summary = "Login a user")
+    @PostMapping("/login")
+    public String login() {
+        return "not implemented";
+    }
 }
