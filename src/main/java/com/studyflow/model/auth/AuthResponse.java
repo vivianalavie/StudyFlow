4f2f701446239b097b1acfbeda1a8d9ddd447181
maurice <maurice.pestka@fs-students.de>
2025-05-01 15:50:20 +0200

    Made again changes to the names of the variables, so that the json response body is handled correctly

--- src/main/java/com/studyflow/model/auth/AuthResponse.java
+++ src/main/java/com/studyflow/model/auth/AuthResponse.java
@@ -1,15 +1,17 @@
 package com.studyflow.model.auth;
 
+import com.google.gson.annotations.SerializedName;
+
 record UserDetails (
     String id,
     String email
 ) {}
 
 public record AuthResponse (
-    String accessToken,
-    String tokenType,
-    int expiresIn,
-    int expiresAt,
-    String refreshToken,
+    @SerializedName("access_token") String accessToken,
+    @SerializedName("token_type") String tokenType,
+    @SerializedName("expires_in") int expiresIn,
+    @SerializedName("expires_at") int expiresAt,
+    @SerializedName("refresh_token") String refreshToken,
     UserDetails user
 ) { }
