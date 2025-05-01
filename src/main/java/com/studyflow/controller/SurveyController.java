792bb1ea25609c43c1609c05ed3f747eb281d56e
maurice <maurice.pestka@fs-students.de>
2025-05-01 19:23:55 +0200

    Build the Endpoint for getting all questions within the quiz_questions table to show them within our question survey after the login

---
+++ src/main/java/com/studyflow/controller/SurveyController.java
@@ -0,0 +1,26 @@
+package com.studyflow.controller;
+
+
+import com.google.gson.Gson;
+import com.studyflow.model.survey.Questions;
+import com.studyflow.repository.SupabaseQuestionRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/survey")
+public class SurveyController {
+
+    private final SupabaseQuestionRepository questionRepository = new SupabaseQuestionRepository();
+
+    @Operation(summary = "Retrieves all active questions for the quiz")
+    @GetMapping("/questions")
+    public List<Questions> getQuestions() {
+        return questionRepository.getAllQuestions(); //Gets all questions from SupaBase
+    }
+
+}
---
+++ src/main/java/com/studyflow/model/survey/Questions.java
@@ -0,0 +1,11 @@
+package com.studyflow.model.survey;
+
+import java.util.List;
+
+public record Questions (
+//        String uuid,
+          String name
+//        List<String> options
+//        boolean active, //If we need it
+//        date created_on
+) {}
