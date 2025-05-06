911be7ac189ba640003e64a85c605ef9a675442b
chiararupprecht <chiara.rupprecht@fs-students.de>
2025-05-06 17:23:46 +0200

    Chiaras Änderungenzu Quiz Antworten

---
+++ src/main/java/com/studyflow/model/survey/Answers.java
@@ -0,0 +1,13 @@
+package com.studyflow.model.survey;
+
+public class quizAnswers {
+
+    {
+        private String question_id;
+        private String choice;
+        private String userId; // optional, wenn man nur eingeloggte Nutzer hat//sollten wir haben??
+        private date createrd_on;
+
+        // Getter und Setter
+    }
+}
