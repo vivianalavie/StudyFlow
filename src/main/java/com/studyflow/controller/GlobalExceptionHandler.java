a525e3b760eee34c321b2135d6b9d895244c588a
vivianalavie <viviana.littig@fs-students.de>
2025-05-07 20:10:49 +0200

    Made error handling changes

--- src/main/java/com/studyflow/controller/GlobalExceptionHandler.java
+++ src/main/java/com/studyflow/controller/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.studyflow.controller;
 
 import com.studyflow.exception.SignupFlowException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(SignupFlowException.class)
     public ResponseEntity<String> handleSignupFlowException(
@@ -36,4 +40,14 @@ public class GlobalExceptionHandler {
                     .body("An unexpected error occurred during signup.");
         }
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        log.error("An unexpected error occurred.");
+        log.error(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred.");
+    }
+
 }
