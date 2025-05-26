3a8a330b39436d104e4771b3f1307389ac5753bc
SysDev-Maurice <maurice.pestka@fs-students.de>
2025-05-26 18:27:57 +0200

    Scrum 68 add or edit assignments (#11)
    * Created the functionality to insert/update/delete assignments --> Build 3 endpoints for this
    
    * Changed the column created_by_user, because of the change of the users table
    
    * Made changes to data model
    
    * Made error handling changes
    
    * Added the possibility to see all of the assignments created by a user
    
    * Added the possibility to see all of the assignments created by a user
    
    ---------
    
    Co-authored-by: vivianalavie <viviana.littig@fs-students.de>

--- src/main/java/com/studyflow/repository/CourseRepository.java
+++ src/main/java/com/studyflow/repository/CourseRepository.java
@@ -20,7 +20,7 @@ public interface CourseRepository {
 
     @SqlUpdate("""
         INSERT INTO courses (
-            created_by, name, description, start_date, end_date,
+            created_by_user_id, name, description, start_date, end_date,
             professor_name, total_points, total_workload_hours, total_self_work_hours
         )
         VALUES (
@@ -41,7 +41,7 @@ public interface CourseRepository {
         total_workload_hours = :totalWorkloadHours,
         total_self_work_hours = :totalSelfWorkHours
     WHERE id = :id
-""")
+    """)
     void updateCourse(@BindBean Course course);
 
     @SqlUpdate("DELETE FROM courses WHERE id = :id")
