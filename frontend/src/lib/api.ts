9ffaceb4c3e7fd976b0b7179efbebbe74929a50d
Anton Klack <anton.klack@fs-students.de>
2025-05-28 14:14:47 +0200

    created quiz, which stores user preferences and connects it to database

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,4 @@
-// frontend/src/lib/api.ts
+
 export interface RegisterDto {
     firstName: string;
     lastName: string;
@@ -6,7 +6,7 @@ export interface RegisterDto {
     password: string;
 }
 
-// Lies dir die BASE_URL aus .env.local ein, mit Fallback
+
 const BASE_URL = process.env.NEXT_PUBLIC_JAVA_BACKEND_URL ?? "http://localhost:8080";
 
 export async function registerUser(data: RegisterDto) {
